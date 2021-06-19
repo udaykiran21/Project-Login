d4849a717c64c03afe21f8366875445890e3b80c
B. Uday Kiran <63153044+udaykiran21@users.noreply.github.com>
2021-06-19 21:37:32 +0530

    Add files via upload

---
+++ user.js
@@ -0,0 +1,16 @@
+// importing modules
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+  
+  
+var UserSchema = new Schema({   
+   
+    username : String
+});
+  
+// plugin for passport-local-mongoose
+UserSchema.plugin(passportLocalMongoose);
+  
+// export userschema
+ module.exports = mongoose.model("User", UserSchema);
